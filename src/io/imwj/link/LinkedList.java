c4f5fa3a66b3fe40f39c4217699cb63f4d1cdef4
LANGAO <2916863213@qq.com>
2020-06-30 22:35:34 +0800

    提交：链表类普通添加元素

--- src/io/imwj/link/LinkedList.java
+++ src/io/imwj/link/LinkedList.java
@@ -32,4 +32,73 @@ public class LinkedList<E> {
                     '}';
         }
     }
+
+    private Node head;
+
+    private int size;
+
+    public LinkedList(){
+        head = null;
+        size = 0;
+    }
+
+    /**
+     * 获取size长度
+     * @return
+     */
+    public int getSize(){
+        return size;
+    }
+
+    /**
+     * 判断是否为空
+     * @return
+     */
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    /**
+     * 在链表头添加元素
+     * 只需要把之前的head添加到新元素e.next后面即可
+     * @param e
+     */
+    public void addFirst(E e){
+        head = new Node(e, head);
+
+        size ++;
+    }
+
+    /**
+     * 在链表指定位置添加元素
+     * 注意 要先把指定index后的元素添加到e.next 再把e添加到index前元素的next下
+     * @param index
+     * @param e
+     */
+    public void add(int index, E e){
+        if(index < 0 || index > size){
+            throw new IllegalArgumentException("add 添加元素失败 === index不合法！");
+        }
+        if(index == 0){
+            addFirst(e);
+        }
+
+        Node prev = head;
+        for(int i=0; i< index - 1; i++){
+            prev = prev.next;
+        }
+        prev.next = new Node(e, prev.next);
+
+        size++;
+    }
+
+    /**
+     * 在链表尾部添加元素
+     * @param e
+     */
+    public void addLast(E e){
+        add(size, e);
+    }
+
+
 }
